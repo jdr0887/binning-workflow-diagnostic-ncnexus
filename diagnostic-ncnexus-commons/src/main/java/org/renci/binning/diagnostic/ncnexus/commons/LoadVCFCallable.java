b3a617ed69894f80046f92944b6100565f2822df
Jason Reilly <jdr0887@gmail.com>
2017-01-26 13:28:01 -0500

    changing irodsutils call...particpant is set in map manually

--- diagnostic-ncnexus-commons/src/main/java/org/renci/binning/diagnostic/ncnexus/commons/LoadVCFCallable.java
+++ diagnostic-ncnexus-commons/src/main/java/org/renci/binning/diagnostic/ncnexus/commons/LoadVCFCallable.java
@@ -88,11 +88,12 @@ public class LoadVCFCallable extends AbstractLoadVCFCallable {
     public File getVCF(String participant) throws BinningException {
         logger.debug("ENTERING getVCF(String)");
         Map<String, String> avuMap = new HashMap<String, String>();
+        avuMap.put("ParticipantId", participant);
         avuMap.put("MaPSeqStudyName", "NCNEXUS");
         avuMap.put("MaPSeqWorkflowName", "NCNEXUSVariantCalling");
         avuMap.put("MaPSeqJobName", "GATKVariantAnnotator");
         avuMap.put("MaPSeqMimeType", "TEXT_VCF");
-        String irodsFile = IRODSUtils.findFile(participant, avuMap);
+        String irodsFile = IRODSUtils.findFile(avuMap);
         String participantDir = String.format("%s/annotation/NCNEXUS/%s", binningDirectory, participant);
         File vcfFile = IRODSUtils.getFile(irodsFile, participantDir);
         return vcfFile;
